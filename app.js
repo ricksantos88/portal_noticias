fbdb7c36e35c111fd5f16421de97977c487ab877
Wendel Santos <henrique.santos1988@gmail.com>
2018-07-20 20:25:18 -0300

    Estudo Node, portal de noticias - primeiro commit

---
+++ app.js
@@ -0,0 +1,5 @@
+var app = require("./config/server");
+
+app.listen(3000, function(){
+    console.log("Servidor executando via express. Porta:3000");
+});
---
+++ config/dbConnection.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+var connMySQL = function() {
+    console.log('>>connection with database was established!!!');
+    return mysql.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: 'portal_noticias'
+    });
+};
+
+module.exports = function() {
+    console.log('>>autoload(consign) loaded the connection to the database(MySQL).');    
+    return connMySQL;
+}
